ab55fce593a113e4edb1e233d0bc391fc9114555
Navneet Pratap <pratap102718@gmail.com>
2026-03-16 19:16:49 +0530

    Add inverted triangle pattern examples with user input in patterns_problems.cpp

--- Basics/Patterns/patterns_problems.cpp
+++ Basics/Patterns/patterns_problems.cpp
@@ -129,3 +129,155 @@ int main(){
 }
 
 
+// Print the Inverted Right Triangle by taking n by user...
+/* 
+
+* * * * * * 
+* * * * * 
+* * * *
+* * *
+* * 
+*
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin>> n ;
+    // for(int i = n; i >= 1; i--) { 
+    //     for(int j = 1; j <=i; j++){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<n-i;j++){
+            cout << "* ";
+        } 
+        cout << endl;
+    }
+    return 0;
+}
+
+// Print the Inverted  Triangle by taking n by user...
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin>> n ;
+            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n -i - 1; j++) {
+                cout << " ";
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    return 0;
+};
+
+
+// #################    Other Approach
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin>> n ;
+            //for (int i = n; i >= 1; i--) {
+            //for (int j = 0; j < n - i; j++) {
+            for(int i = 0; i < n; i++){
+                for(int j=0;j<i;j++){
+                cout << " ";
+            }
+            //for (int j = 0; j < 2 * i - 1; j++) {
+            for (int j = 0; j<2*n - (2*i + 1); j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    return 0;
+};
+
+/*  Print this pattern
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin>> n ;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+            }
+            cout << endl;
+        }
+        for(int i=n-1; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                cout<<"* ";
+            }
+            cout << endl;
+        }
+};
+
+// #########################  Other Approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin>> n ;
+    for(int i=1;i<=2*n-1;i++){
+        int stars = i;
+        if(i>n){
+            stars = 2 * n - i;
+        }
+        for(int j=1;j<=stars;j++){
+            cout<< "* ";
+        }
+        cout << endl;
+    }   
+    return 0;
+};
+
+
+/* Print this pattern
+1                 1 
+1 2             2 1 
+1 2 3         3 2 1 
+1 2 3 4     4 3 2 1
+1 2 3 4 5 5 4 3 2 1
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value of n: ";
+    cin>> n ;
+            for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<< j<<" ";
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                cout << "  ";
+            }
+            for(int j=i;j>=1;j--){
+                cout<< j<<" ";
+            }
+            cout <<endl;
+        }
+    return 0;
+};
+
